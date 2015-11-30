21ae83092fe2ecf61f2dcb79f333df90e58bc923
Mike Tangolics <mtangolics@gmail.com>
2015-11-30 11:12:39 -0500

    Initial Commit

---
+++ BlackjackX/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BlackjackX
+//
+//  Created by Mike Tangolics on 11/30/15.
+//  Copyright (c) 2015 Mike Tangolics. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
